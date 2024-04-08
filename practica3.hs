d127fd0d5c947f4c518fc6573ddb84a3872ba3cd
MartoNievas <tinnievas@gmail.com>
2024-04-08 07:46:38 -0300

    funciones compuestas

--- practica3.hs
+++ practica3.hs
@@ -7,8 +7,17 @@ f n | n == 1 = 8
     | n == 4 = 131
     | n == 16 = 16
 
+-- Funcion f que le meto un entero y devuelve otro
 
 g :: Int -> Int 
 g n | n == 8 = 16 
     | n == 16 = 4 
     | n == 131 = 1
+
+    -- funciones compuestas h y k 
+
+h1::Int -> Int
+h1 x = f (g x)
+
+k1 :: Int -> Int
+k1 z = g (f z)
