585a7be83c60c894c88923aa74bc4fdb7ce19e77
MartoNievas <tinnievas@gmail.com>
2024-04-08 07:34:11 -0300

    Segunda funcion g

--- practica3.hs
+++ practica3.hs
@@ -1,6 +1,14 @@
 module Uno where 
 
+--Funcion f que le meto un entero y devuelve otro 
+
 f :: Int -> Int 
 f n | n == 1 = 8
     | n == 4 = 131
     | n == 16 = 16
+
+
+g :: Int -> Int 
+g n | n == 8 = 16 
+    | n == 16 = 4 
+    | n == 131 = 1
